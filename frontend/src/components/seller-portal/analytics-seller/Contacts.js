333c9b755a7a2cfe520c9c875130b6e26775513b
Parth Patel <kush_312@yahoo.com>
2023-07-26 13:09:11 -0300

    added comments for front-end

--- frontend/src/components/seller-portal/analytics-seller/Contacts.js
+++ frontend/src/components/seller-portal/analytics-seller/Contacts.js
@@ -1,6 +1,12 @@
 //Created by Parth Patel
+// Referred to the youtube video on how to use socket.io for real time commjunication and partially used the code after understanding to implement the chat feature.
+// URL1: https://www.youtube.com/watch?v=otaQKODEUFs&t=13178s&ab_channel=KishanSheth
+// URL2: https://github.com/koolkishan/chat-app-react-nodejs
+// Date Accessed: 07/26/2023
+// Used by Parth Patel
 import React, { useState } from "react";
 
+// component to display individual contacts in the contact list
 function Contact({ contact, isSelected, onClick }) {
   return (
     <div
@@ -14,9 +20,11 @@ function Contact({ contact, isSelected, onClick }) {
   );
 }
 
+// Main Contacts component to display the list of contacts
 export default function Contacts({ contacts, changeChat }) {
   const [currentSelected, setCurrentSelected] = useState(null);
 
+  //Function to change the currently selected chat and trigger the chat change
   const changeCurrentChat = (index, contact) => {
     setCurrentSelected(index);
     changeChat(contact);
