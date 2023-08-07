0bc473dfc2440498dc4057d738279994bc28e7ec
Patrick <pt308649@dal.ca>
2023-08-06 22:14:07 -0300

    removed cart.length where it should not be

--- frontend/src/components/buyers-portal/orders-buyers/cart/cart.js
+++ frontend/src/components/buyers-portal/orders-buyers/cart/cart.js
@@ -197,7 +197,7 @@ const Cart = () => {
       <Grid container rowSpacing={1} alignItems="center" justifyContent="center">
         <Grid item xs={12} alignItems="center">
           <Grid container justifyContent="center">
-            <h1  className="checkoutHeading">Cart {payments.length}</h1>
+            <h1  className="checkoutHeading">Cart </h1>
           </Grid>
         </Grid>
         <Grid item xs={12}>
